bed1b67474b59116d39e32c89a47c64c2ae9240e
o-jill <nobaokix@gmail.com>
2022-06-22 22:11:07 +0900

    added read kifu from a file.

--- src/kifu.rs
+++ src/kifu.rs
@@ -3,6 +3,7 @@ use super::*;
 pub const SENTEWIN : i8 = 1;
 pub const DRAW : i8 = 0;
 pub const GOTEWIN : i8 = -1;
+pub const STR_POSX : &str = "0abcdefgh";
 
 pub struct Te {
     x : usize,
@@ -21,11 +22,40 @@ impl Te {
         }
     }
 
+    pub fn from(line : &str) -> Option<Te> {
+        let elem = line.split_whitespace().collect::<Vec<&str>>();
+        // nth teban posxy rfen rfen-teban
+        if elem.len() != 5 {
+            return None;
+        }
+        let x : usize;
+        let y : usize;
+        if elem[2] == "PS" {
+            x = 0;
+            y = 0;
+        } else {
+            let c = elem[2].chars().nth(0).unwrap();
+            let ox = STR_POSX.find(c);
+            if ox.is_none() {
+                return None;
+            }
+            x = ox.unwrap();
+            y = elem[2].chars().nth(1).unwrap().to_digit(10).unwrap() as usize;
+        }
+        let rfen = format!("{} {}", elem[3], elem[4]);
+        Some(Te {
+            x : x,
+            y : y,
+            teban : 0,
+            rfen : rfen
+        })
+    }
+
     pub fn pos(&self) -> String {
         if self.x == 0 || self.y == 0 {
             return String::from("PS")
         }
-        format!("{}{}", board::STR_GOTE.chars().nth(self.x).unwrap(), self.y)
+        format!("{}{}", STR_POSX.chars().nth(self.x).unwrap(), self.y)
     }
 
     pub fn to_str(&self, i : usize) -> String {
@@ -51,6 +81,30 @@ impl Kifu {
             list : Vec::<Te>::new(),
             score : None,
         }
+    }
+
+    pub fn from(lines : &Vec<&str>) -> Kifu {
+        let mut ret = Kifu {
+            list : Vec::<Te>::new(),
+            score : None,
+        };
+        for &l in lines {
+            let te = Te::from(&l);
+            if te.is_none() {
+                continue;
+            }
+            ret.list.push(te.unwrap());
+        }
+        // score?
+        let result = lines.last().unwrap();
+        if result.find("SENTE won.").is_some() {
+            ret.score = Some(1);
+        } else if result.find("GOTE won.").is_some() {
+            ret.score = Some(-1);
+        } else if result.find("DRAW.").is_some() {
+            ret.score = Some(0);
+        }
+        ret
     }
 
     pub fn append(&mut self, x : usize, y : usize, t : i8, rfen : String) {
