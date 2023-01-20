f93b26e8580e94e63bad0c6404954e2d5089942d
o-jill <nobaokix@gmail.com>
2023-01-20 15:56:54 +0900

    added fn to play w/ one tree.

--- src/main.rs
+++ src/main.rs
@@ -312,19 +312,20 @@ fn duel(ev1 : &str, ev2 : &str) {
             let think = MYOPT.get().unwrap().think.as_str();
             match think {
                 "" | "ab" => {
-                    g.start_with_2et(
-                        // nodebb::NodeBB::think_ab_extract3,
-                        // nodebb::NodeBB::think_ab_extract2,
-                        nodebb::NodeBB::think_ab,
-                        7, &w1, &w2).unwrap()
+                    // g.start_with_2et(
+                    //     // nodebb::NodeBB::think_ab_extract3,
+                    //     // nodebb::NodeBB::think_ab_extract2,
+                    //     nodebb::NodeBB::think_ab,
+                    //     7, &w1, &w2).unwrap()
+                    g.starto_with_2et(nodebb::NodeBB::thinko_ab, 7, &w2, &w1).unwrap()
                 },
                 "all" => {
-                    // g.starto_with_2et(nodebb::NodeBB::thinko, 7, &w2, &w1).unwrap()
-                    g.start_with_2et(nodebb::NodeBB::think, 7, &w1, &w2).unwrap()
+                    g.starto_with_2et(nodebb::NodeBB::thinko, 7, &w2, &w1).unwrap()
+                    // g.start_with_2et(nodebb::NodeBB::think, 7, &w1, &w2).unwrap()
                 },
                 // "" => {
-                //     g.startsh_with_2et(shnode::ShNode::think_ab_extract2, 7, &w1, &w2).unwrap()
-                //     // g.startsh_with_2et(shnode::ShNode::think_ab, 7, &w1, &w2).unwrap()
+                //     // g.startsh_with_2et(shnode::ShNode::think_ab_extract2, 7, &w1, &w2).unwrap()
+                //     g.startsh_with_2et(shnode::ShNode::think_ab, 7, &w1, &w2).unwrap()
                 //     // g.startsh_with_2et(shnode::ShNode::think, 7, &w1, &w2).unwrap()
                 // },
                 _ => { panic!("unknown thinking method.") }
@@ -367,15 +368,16 @@ fn duel(ev1 : &str, ev2 : &str) {
             match think {
                 "" | "ab" => {
                     // g.start_with_2et(nodebb::NodeBB::think_ab_extract2, 7, &w1, &w2).unwrap()
-                    g.start_with_2et(nodebb::NodeBB::think_ab, 7, &w2, &w1).unwrap()
+                    // g.start_with_2et(nodebb::NodeBB::think_ab, 7, &w2, &w1).unwrap()
+                    g.starto_with_2et(nodebb::NodeBB::thinko_ab, 7, &w2, &w1).unwrap()
                 },
                 "all" => {
-                    // g.starto_with_2et(nodebb::NodeBB::thinko, 7, &w2, &w1).unwrap()
-                    g.start_with_2et(nodebb::NodeBB::think, 7, &w2, &w1).unwrap()
+                    g.starto_with_2et(nodebb::NodeBB::thinko, 7, &w2, &w1).unwrap()
+                    // g.start_with_2et(nodebb::NodeBB::think, 7, &w2, &w1).unwrap()
                 },
                 // "" => {
-                //     g.startsh_with_2et(shnode::ShNode::think_ab_extract2, 7, &w2, &w1).unwrap()
-                //     // g.startsh_with_2et(shnode::ShNode::think_ab, 7, &w2, &w1).unwrap()
+                //     // g.startsh_with_2et(shnode::ShNode::think_ab_extract2, 7, &w2, &w1).unwrap()
+                //     g.startsh_with_2et(shnode::ShNode::think_ab, 7, &w2, &w1).unwrap()
                 //     // g.startsh_with_2et(shnode::ShNode::think, 7, &w2, &w1).unwrap()
                 // },
                 _ => { panic!("unknown thinking method.") }
