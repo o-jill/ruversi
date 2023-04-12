c9ffa17a6ddea5b0853c8306648dbe732946e7bc
o-jill <nobaokix@gmail.com>
2023-04-12 21:13:55 +0900

    added duel against Edax.

--- src/main.rs
+++ src/main.rs
@@ -610,15 +610,13 @@ fn duel(ev1 : &str, ev2 : &str, duellv : i8, depth : u8) {
 
 /// duel between 2 eval tables.
 /// # Arguments
-/// - ev1 : eval table 1.
-/// - ev2 : eval table 2.
-fn duel_vs_edax(ev1 : &str, duellv : i8, depth : u8) {
+/// - duellv : duel level.
+/// - depth : searching depth.
+fn duel_vs_edax(duellv : i8, depth : u8) {
     if duellv < 3 || 13 < duellv {
         panic!("duel level:{duellv} is not supported...");
     }
 
-    let mut w1 = weight::Weight::new();
-    w1.read(ev1).unwrap();
     let mut win = [0, 0];
     let mut draw = [0, 0];
     let mut lose = [0, 0];
@@ -766,8 +764,8 @@ fn duel_vs_edax(ev1 : &str, duellv : i8, depth : u8) {
         let winrate = 100.0 * twin as f64 / (total - tdraw) as f64;
         let r = 400.0 * (twin as f64 / tlose as f64).log10();
         println!("total,{total},win,{twin},draw,{tdraw},lose,{tlose},balance,{tsen},{tgo},{winrate:.2}%,R,{r:+.1}");
-        println!("ev1 @@,win,{},draw,{},lose,{}", win[0], draw[0], lose[0]);
-        println!("ev1 [],win,{},draw,{},lose,{}", win[1], draw[1], lose[1]);
+        println!("ext @@,win,{},draw,{},lose,{}", win[0], draw[0], lose[0]);
+        println!("ext [],win,{},draw,{},lose,{}", win[1], draw[1], lose[1]);
     }
 }
 
@@ -1075,6 +1073,10 @@ fn main() {
         let ev2 = &MYOPT.get().unwrap().evaltable2;
         let duellv = MYOPT.get().unwrap().duellv;
         duel(ev1, ev2, duellv, depth);
+    }
+    if *mode == myoption::Mode::DuelExt {
+        let duellv = MYOPT.get().unwrap().duellv;
+        duel_vs_edax(duellv, depth);
     }
     if *mode == myoption::Mode::Play {
         let turn = MYOPT.get().unwrap().turn;
