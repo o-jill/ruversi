b069d17c0fd21a1997eb44d6b19cea1c99be5c64
o-jill <nobaokix@gmail.com>
2023-06-09 13:19:18 +0900

    added evaluating cells w/ i32-avx.

--- src/weight.rs
+++ src/weight.rs
@@ -1145,6 +1145,7 @@ if brd_int {
         let white = ban.white;
         let teban = ban.teban as f32;
         let ow = &self.weight;
+        let iow = &self.iweight;
 
         let fs = ban.fixedstones();
 
@@ -1161,7 +1162,104 @@ if brd_int {
         for i in 0..N_HIDDEN / N {
             let hidx = i * N;
             let mut sum48 : [f32 ; N * 8] = [0.0 ; N * 8];
+let brd_int = false;
+// let brd_int = true;
+if brd_int {
+    for n in 0..N {
+        let res8 = sum48[n * 8..].as_mut_ptr();
+        let w1 = &iow[(hidx + n) * board::CELL_2D .. (hidx + n + 1) * board::CELL_2D];
+        // let mut hidsum : f32 = dc[i];
+        let mut sum8: x86_64::__m256i;
+        unsafe {
+            sum8 = x86_64::_mm256_setzero_si256();
+        }
+        const M : usize = 32;
+        let mut bit8 : u64 = 0x0101010101010101;
+        for j in 0..board::CELL_2D / M {
+            let idx = j * M;
+            let b81 = (bit8 & black) >> 4 * j;
+            let w81 = (bit8 & white) >> 4 * j;
+            bit8 <<= 1;
+            let b82 = (bit8 & black) >> 4 * j + 1;
+            let w82 = (bit8 & white) >> 4 * j + 1;
+            bit8 <<= 1;
+            let b83 = (bit8 & black) >> 4 * j + 2;
+            let w83 = (bit8 & white) >> 4 * j + 2;
+            bit8 <<= 1;
+            let b84 = (bit8 & black) >> 4 * j + 3;
+            let w84 = (bit8 & white) >> 4 * j + 3;
+            bit8 <<= 1;
 
+            unsafe {
+                // 0x00000000ffeeddcc000000007766554400000000bbaa99880000000033221100
+                let b32 = x86_64::_mm256_set_epi64x(
+                    (b82 >> 32) as i64, (b81 >> 32) as i64,
+                    (b82 & 0xffffffff) as i64, (b81 & 0xffffffff) as i64);
+                let w32 = x86_64::_mm256_set_epi64x(
+                    (w82 >> 32) as i64, (w81 >> 32) as i64,
+                    (w82 & 0xffffffff) as i64, (w81 & 0xffffffff) as i64);
+                let b322 = x86_64::_mm256_set_epi64x(
+                    (b84 >> 32) as i64, (b83 >> 32) as i64,
+                    (b84 & 0xffffffff) as i64, (b83 & 0xffffffff) as i64);
+                let w322 = x86_64::_mm256_set_epi64x(
+                    (w84 >> 32) as i64, (w83 >> 32) as i64,
+                    (w84 & 0xffffffff) as i64, (w83 & 0xffffffff) as i64);
+
+                // -1 or 0 or +1
+                let c321 = x86_64::_mm256_sub_epi8(b32, w32);
+                let c322 = x86_64::_mm256_sub_epi8(b322, w322);
+
+                let zero = x86_64::_mm256_setzero_si256();
+                // to i16
+                let s321 = x86_64::_mm256_cmpgt_epi8(zero, c321);
+                let s322 = x86_64::_mm256_cmpgt_epi8(zero, c322);
+                // 0x0000000000000000777766665555444400000000000000003333222211110000
+                let c161 = x86_64::_mm256_unpacklo_epi8(c321, s321);
+                // 0x0000000000000000ffffeeeeddddcccc0000000000000000bbbbaaaa99998888
+                let c162 = x86_64::_mm256_unpackhi_epi8(c321, s321);
+                // 0x0000000000000000777766665555444400000000000000003333222211110000
+                let c163 = x86_64::_mm256_unpacklo_epi8(c322, s322);
+                // 0x0000000000000000ffffeeeeddddcccc0000000000000000bbbbaaaa99998888
+                let c164 = x86_64::_mm256_unpackhi_epi8(c322, s322);
+
+                // to i32
+                // 0x7777777766666666555555554444444433333333222222221111111100000000
+                let s161 = x86_64::_mm256_cmpgt_epi16(zero, c161);
+                let c81 = x86_64::_mm256_unpacklo_epi16(c161, s161);
+                // 0xffffffffeeeeeeeeddddddddccccccccbbbbbbbbaaaaaaaa9999999988888888
+                let s162 = x86_64::_mm256_cmpgt_epi16(zero, c162);
+                let c82 = x86_64::_mm256_unpacklo_epi16(c162, s162);
+                // 0x7777777766666666555555554444444433333333222222221111111100000000
+                let s163 = x86_64::_mm256_cmpgt_epi16(zero, c163);
+                let c83 = x86_64::_mm256_unpacklo_epi16(c163, s163);
+                // 0xffffffffeeeeeeeeddddddddccccccccbbbbbbbbaaaaaaaa9999999988888888
+                let s164 = x86_64::_mm256_cmpgt_epi16(zero, c164);
+                let c84 = x86_64::_mm256_unpacklo_epi16(c164, s164);
+
+                let x81 = x86_64::_mm256_loadu_si256(w1.as_ptr().add(idx) as *const x86_64::__m256i);
+                let x82 = x86_64::_mm256_loadu_si256(w1.as_ptr().add(idx + 8) as *const x86_64::__m256i);
+                let x83 = x86_64::_mm256_loadu_si256(w1.as_ptr().add(idx + 16) as *const x86_64::__m256i);
+                let x84 = x86_64::_mm256_loadu_si256(w1.as_ptr().add(idx + 24) as *const x86_64::__m256i);
+
+                let mul1 = x86_64::_mm256_mul_epi32(x81, c81);
+                let mul2 = x86_64::_mm256_mul_epi32(x82, c82);
+                let mul3 = x86_64::_mm256_mul_epi32(x83, c83);
+                let mul4 = x86_64::_mm256_mul_epi32(x84, c84);
+
+                sum8 = x86_64::_mm256_add_epi32(mul1, sum8);
+                sum8 = x86_64::_mm256_add_epi32(mul2, sum8);
+                sum8 = x86_64::_mm256_add_epi32(mul3, sum8);
+                sum8 = x86_64::_mm256_add_epi32(mul4, sum8);
+            }
+        }
+        unsafe {
+            let sm = x86_64::_mm256_cvtepi32_ps(sum8);
+            let mag = x86_64::_mm256_set1_ps(1.0 / MAG_F32_TO_I32);
+            let w8 = x86_64::_mm256_mul_ps(mag, sm);
+            x86_64::_mm256_storeu_ps(res8, w8);
+        }
+    }
+} else {
             for n in 0..N {
                 let res8 = sum48[n * 8..].as_mut_ptr();
                 let w1 = &ow[(hidx + n) * board::CELL_2D .. (hidx + n + 1) * board::CELL_2D];
@@ -1264,7 +1362,7 @@ if brd_int {
                     x86_64::_mm256_storeu_ps(res8, sum8);
                 }
             }
-
+}
             unsafe {
                 let x11 = x86_64::_mm_load_ps(sum48.as_ptr());
                 let x12 = x86_64::_mm_load_ps(sum48.as_ptr().add(4));
