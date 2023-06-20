9a0890a72c5999beb293b55433ebb8b5bf877602
o-jill <nobaokix@gmail.com>
2023-06-21 08:48:19 +0900

    added measuring learning speed.

--- tools/speedcheck.rb
+++ tools/speedcheck.rb
@@ -1,7 +1,7 @@
 # example:
 #  $ ruby speedcheck.rb < speedcheck.txt
 
-def read(lines)
+def search(lines)
     values = []
     nodes = 0
     lines.each {|line|
@@ -22,10 +22,60 @@ def read(lines)
             sd = Math.sqrt(vari)
             min, max = values.minmax
             puts "speed: #{'%.2f' % (nodes / avg)} nodes/msec"
-            puts "#{nodes} nodes / #{'%.2f' % avg} +- #{'%.2f' % sd} msec (#{min} -- #{max})"
+            puts "#{nodes} nodes / #{'%.2f' % avg} ± #{'%.2f' % sd} msec (#{min} -- #{max})"
             next
         end
     }
 end
 
-read(readlines)
+def learn(lines)
+    values = []
+    files = 0
+    repeat = 0
+    lines.each {|line|
+        if line.start_with?("processing time:")
+# processing time: 0min 6.0sec (602.6msec/batch, 0.069msec/file)
+            m = / (\d+)min (\d+\.\d)sec/.match(line)
+            values.push(m[1].to_i * 60 + m[2].to_f)
+            next
+        elsif line.start_with?("total,")
+# total,8687,win,1630,draw,56,lose,7001
+            m = /total.(\d+),/.match(line)
+            files = m[1].to_i
+            next
+        elsif line =~ /^\d+ \/ \d+\r/
+            repeat = line.split(' ')[-1].to_i
+            next
+        end
+    }
+    avg = values.sum.fdiv(values.length)
+    vari = values.map {|elem| elem * elem}.sum.fdiv(values.length) - avg * avg
+    sd = Math.sqrt(vari)
+    min, max = values.minmax
+    puts "#{'%.1f' % avg } ± #{'%.1f' % sd} sec (#{min} -- #{max}) for #{files} files x #{repeat} repeats"
+    print "speed: #{'%.1f' % (avg * 1000.0 / repeat)} msec/batch, "
+    puts "#{'%.1f' % (avg * 1000000.0 / repeat / files)} usec/file"
+end
+
+def help()
+    puts "ruby #{__FILE__} <mode>"
+    puts "  search : summarize searching speed."
+    puts "  learn : summarize learning speed."
+    puts "  game : not yet."
+    puts "  help : show this help."
+end
+
+# main
+md = ARGV[0] || "help"
+
+if md == "search"
+    search($stdin.read.split("\n"))
+elsif md == "learn"
+    learn($stdin.read.split("\n"))
+elsif md == "game"
+    help()
+elsif md == "help"
+    help()
+else
+    help()
+end
--- tools/speedcheck.sh
+++ tools/speedcheck.sh
@@ -1,20 +1,63 @@
 #!/bin/bash -x
 
-
-DEPTH=11
+# REPEAT=5
+REPEAT=20
 
 DATESTR=`date +%Y%m%d%H%M%S`
 RESULT=speedcheck${DATESTR}.txt
+
+search() {
+# SDEPTH="--depth 9"
+SDEPTH="--depth 11"
+# FEATURES=""  # sse
+FEATURES="--features=avx"
 
 RFENS=("8/8/8/3Aa3/3aA3/8/8/8 b" "8/8/8/3aA3/3Aa3/8/8/8 b" "A1A1A3/1c4/Aa1dA/1c4/A1a1a3/2a2a2/2a3a1/2A4A b")
 LENRFENS=${#RFENS[@]}
 
 for ((i=0;i<${LENRFENS};i++)) do
   echo "Begin RFEN:${RFENS[$i]}" >> ${RESULT}
-  for ((j=0;j<20;j++)) do
-    RUSTFLAGS="-Ctarget-cpu=native" cargo run --release --features=avx -- --rfen "${RFENS[$i]}" --depth ${DEPTH} >> ${RESULT} 2>/dev/null
+  for ((j=0;j<${REPEAT};j++)) do
+    RUSTFLAGS="-Ctarget-cpu=native" cargo run --release ${FEATURES} -- --rfen "${RFENS[$i]}" ${SDEPTH} >> ${RESULT} 2>/dev/null
   done
   echo "End RFEN:${RFENS[$i]}" >> ${RESULT}
 done
 
-cat ${RESULT} | ruby ./tools/speedcheck.rb
+cat ${RESULT} | ruby ./tools/speedcheck.rb search
+}
+
+learn() {
+LREPEAT="--repeat 20"
+KIFUFILE="kifu/kifu0000000.txt"
+
+  if ! [ -e $KIFUFILE ]; then
+    echo "a kifu file was not found."
+    echo "please prepare kifu files in ./kifu/ ."
+    exit
+  fi
+#  echo "a kifu file was found."
+  for ((j=0;j<${REPEAT};j++)) do
+    RUSTFLAGS="-Ctarget-cpu=native" cargo run --release --features=avx -- --learn ${LREPEAT} >> ${RESULT} 2>/dev/null
+  done
+
+cat ${RESULT} | ruby ./tools/speedcheck.rb learn
+}
+
+help() {
+  echo "$0 <mode>"
+  echo "mode:"
+  echo "  search : measure searching speed."
+  echo "  learn : measure learning speed."
+  echo "  game : not yet."
+  echo "  help : show this help."
+}
+
+if [ "$1" = "search" ] ; then
+  search
+elif [ "$1" = "learn" ]; then
+  learn
+elif [ "$1" = "game" ]; then
+  help
+else
+  help
+fi
