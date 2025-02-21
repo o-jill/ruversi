80090c60b9d29de11531b78c9a6265371f819935
o-jill <nobaokix@gmail.com>
2025-02-21 10:11:41 +0900

    trying to add othello engine protocol server communications.

--- src/cassio.rs
+++ src/cassio.rs
@@ -1,5 +1,7 @@
 use super::*;
 use std::fs::OpenOptions;
+use std::io::{BufRead, Read};
+use std::process::{ChildStdin, ChildStdout};
 use std::thread::spawn;
 use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
 
@@ -182,5 +184,139 @@ eprintln!("val:{:?} {} {}msec", val, node.dump(), ft.as_millis());
             }
         }
         Ok(String::from("Done."))
+    }
+}
+
+pub struct OthelloEngineProtocolServer {
+    ply1 : Option<std::process::Child>,
+    ply2 : Option<std::process::Child>,
+    turn : i8,
+}
+
+impl OthelloEngineProtocolServer {
+    pub fn new1(ch : std::process::Child) -> Self {
+        OthelloEngineProtocolServer {
+            ply1 : Some(ch),
+            ply2 : None,
+            turn : board::NONE,
+        }
+    }
+
+    pub fn new2(ch1 : std::process::Child, ch2 : std::process::Child) -> Self {
+        OthelloEngineProtocolServer {
+            ply1 : Some(ch1),
+            ply2 : Some(ch2),
+            turn : board::NONE,
+        }
+    }
+
+    fn setturn(&mut self, trn : i8) {self.turn = trn;}
+
+    fn selectplayer(&mut self) -> Result<&mut std::process::Child, String> {
+        if self.turn == board::NONE {
+            return Err("turn is NONE!".to_string());
+        }
+
+        Ok(if self.turn == board::SENTE {
+            self.ply1.as_mut().unwrap()
+        } else {
+            self.ply2.as_mut().unwrap()
+        })
+    }
+
+    fn getio(&mut self) -> Result<(&mut ChildStdin, &mut ChildStdout), String> {
+        let ch = self.selectplayer()?;
+        let toeng = if let Some(toeng) = ch.stdin.as_mut() {
+            toeng
+        } else {
+            return Err("failed  to get to-engine pipe..".to_string());
+        };
+        let fromeng = if let Some(fromeng) = ch.stdout.as_mut() {
+            fromeng
+        } else {
+            return Err("failed  to get from-engine pipe..".to_string());
+        };
+        Ok((toeng, fromeng))
+    }
+
+    fn init(&mut self) -> Result<(), String> {
+        let (toeng, fromeng) = self.getio()?;
+
+        toeng.write("ENGINE-PROTOCOL init\n".as_bytes());
+
+        std::thread::sleep(std::time::Duration::from_millis(1));
+
+        let mut bufreader = std::io::BufReader::new(fromeng);
+        let mut buf = String::default();
+        bufreader.read_line(&mut buf);
+        if buf == "ready.\n" {
+            return Ok(())
+        }
+        Err(format!("unknown response : \"{buf}\""))
+    }
+
+    fn get_version(&mut self) -> Result<String, String> {
+        let (toeng, fromeng) = self.getio()?;
+
+        toeng.write("ENGINE-PROTOCOL get-version\n".as_bytes());
+
+        std::thread::sleep(std::time::Duration::from_millis(1));
+
+        let mut bufreader = std::io::BufReader::new(fromeng);
+        let mut buf = String::default();
+        bufreader.read_line(&mut buf);
+        let ret = buf.trim().to_string();
+        bufreader.read_line(&mut buf);
+        if buf == "ready.\n" {
+            return Ok(ret)
+        }
+        Err(format!("unknown response : \"{buf}\""))
+    }
+
+    // new-position
+    // midgame-search
+    // endgame-search
+    // get-serach-infos
+
+    fn stop(&mut self) -> Result<(), String> {
+        let (toeng, fromeng) = self.getio()?;
+
+        toeng.write("ENGINE-PROTOCOL stop\n".as_bytes());
+
+        std::thread::sleep(std::time::Duration::from_millis(1));
+
+        let mut bufreader = std::io::BufReader::new(fromeng);
+        let mut buf = String::default();
+        bufreader.read_line(&mut buf);
+        if buf == "ready.\n" {
+            return Ok(())
+        }
+        Err(format!("unknown response : \"{buf}\""))
+    }
+
+    fn empty_hash(&mut self) -> Result<(), String> {
+        let (toeng, fromeng) = self.getio()?;
+
+        toeng.write("ENGINE-PROTOCOL empty-hash\n".as_bytes());
+
+        std::thread::sleep(std::time::Duration::from_millis(1));
+
+        let mut bufreader = std::io::BufReader::new(fromeng);
+        let mut buf = String::default();
+        bufreader.read_line(&mut buf);
+        if buf == "ready.\n" {
+            return Ok(())
+        }
+        Err(format!("unknown response : \"{buf}\""))
+    }
+
+    fn quit(&mut self) -> Result<(), String> {
+        let (toeng, _fromeng) = self.getio()?;
+
+        toeng.write("ENGINE-PROTOCOL quit\n".as_bytes());
+
+        std::thread::sleep(std::time::Duration::from_millis(1));
+
+        Ok(())
     }
 }
