8f7017579c09591d3b0a7f560fbc393b7c406b89
o-jill <nobaokix@gmail.com>
2025-07-22 08:34:00 +0900

    fixed benchmark.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -8,9 +8,9 @@ use mylib::bitboard::BitBoard;
 fn criterion_benchmark_weight(c : &mut Criterion) {
     let w = Weight::new();
     let ban = BitBoard::new();
-    c.bench_function("weight_nosimd", |b| b.iter(|| w.evaluatev7bb(black_box(&ban))));
-    c.bench_function("weight_simd_sse", |b| b.iter(|| w.evaluatev7bb_simd(black_box(&ban))));
-    c.bench_function("weight_simd_avx", |b| b.iter(|| w.evaluatev7bb_simdavx(black_box(&ban))));
+    c.bench_function("weight_nosimd", |b| b.iter(|| w.evaluatev9bb(black_box(&ban))));
+    c.bench_function("weight_simd_sse", |b| b.iter(|| w.evaluatev9bb_simd(black_box(&ban))));
+    c.bench_function("weight_simd_avx", |b| b.iter(|| w.evaluatev9bb_simdavx(black_box(&ban))));
 }
 
 #[cfg(target_arch="aarch64")]
@@ -19,10 +19,10 @@ fn criterion_benchmark_weight(c : &mut Criterion) {
     let ban = BitBoard::new();
     c.bench_function(
         "weight_nosimd",
-        |b| b.iter(|| w.evaluatev7bb(black_box(&ban))));
+        |b| b.iter(|| w.evaluatev9bb(black_box(&ban))));
     c.bench_function(
         "weight_simd_neon_mul",
-        |b| b.iter(|| w.evaluatev7bb_simd_mul(black_box(&ban))));
+        |b| b.iter(|| w.evaluatev9bb_simd_mul(black_box(&ban))));
 }
 
 criterion_group!(benches, criterion_benchmark_weight);
