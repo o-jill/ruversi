43908e88cca6229c546b1c7cd39af9a239959829
o-jill <nobaokix@gmail.com>
2025-08-17 17:55:35 +0900

    renewaled transposition table.

--- src/transptable.rs
+++ src/transptable.rs
@@ -1,4 +1,4 @@
-// use super::*;
+use super::*;
 
 // use std::sync::Mutex;
 
@@ -8,308 +8,112 @@
 //     unsafe {MLOCK = Some(Mutex::new(TranspositionTable::new()))};
 // }
 
-#[derive(Clone)]
+const MAXSIZE : usize = if cfg!(feature="withtt") {1024 * 1024 * 1} else {1};
+
+#[repr(C)]
+#[derive(Clone, Copy)]
 struct TTEntry {
-    pub id : [u8 ; 16],
+    pub hash : u64,
+    pub black : u64,
+    pub white : u64,
     pub hyoka : f32,
-    pub hit : u32,  // ~ 4G leaves
+    pub hit : u16,
+    pub teban : i8,
+}
+
+impl Default for TTEntry {
+    fn default() -> Self {
+        TTEntry::new(0, 0, 0, 0, 9999f32)
+    }
 }
 
 impl TTEntry {
-    pub fn new(i : &[u8 ; 16], hy : f32, hi : u32) -> TTEntry {
-        TTEntry {
-            id : *i,
-            hyoka : hy,
-            hit : hi,
+    pub fn new(hash : u64, black : u64, white : u64, teban : i8, hyoka : f32) -> Self {
+        Self {
+            hash,
+            black,
+            white,
+            hyoka,
+            hit : 0,
+            teban,
         }
+    }
+
+    pub fn from(hash : u64, b : &bitboard::BitBoard, hyoka : f32) -> Self {
+        Self::new(hash, b.black, b.white, b.teban, hyoka)
+    }
+
+    pub fn is_hit(&self, b : &bitboard::BitBoard) -> bool {
+        self.black == b.black && self.white == b.white && self.teban == b.teban
+    }
+
+    pub fn is_hash(&self, hash : u64, teban : i8) -> bool {
+        self.hash == hash && self.teban == teban
     }
 }
 
 pub struct TranspositionTable {
     list : Vec<TTEntry>,
-    sz : usize,
-    nhit : usize,
-    nmiss : usize,
 }
 
-const MAXSIZE : usize = 1024 * 1024;
-const ENTRYSIZE : usize = 1024 * 16;
+impl Default for TranspositionTable {
+    fn default() -> Self {
+        TranspositionTable::new()
+    }
+}
 
 impl TranspositionTable {
-    pub fn new() -> TranspositionTable {
-        TranspositionTable {
-            list : vec![TTEntry::new(&[0 ; 16], -9999.9, 0); ENTRYSIZE],
-            sz : 0,
-            nhit : 0,
-            nmiss : 0,
-        }
+    pub fn new() -> Self {
+        Self::with_capacity(MAXSIZE)
     }
 
-    #[allow(dead_code)]
+    pub fn with_capacity(sz : usize) -> Self {
+        Self { list: vec![TTEntry::default() ; sz] }
+    }
+
     pub fn clear(&mut self) {
         for l in self.list.iter_mut() {
-            l.hit = 0;
-        }
-        self.sz = 0;
-        self.nhit = 0;
-        self.nmiss = 0;
-    }
-
-    pub fn check_or_append<F>(&mut self, id : &[u8 ; 16], mut f : F) -> f32 where
-            F : FnMut() -> f32 {
-        let ttl = &mut self.list;
-        for i in 0..self.sz {
-            if ttl[i].id.cmp(id).is_eq() {
-                self.nhit += 1;
-                ttl[i].hit += 1;
-                let ret = ttl[i].hyoka;
-                for j in (0 .. i).rev() {
-                    if ttl[j].hit < ttl[j + 1].hit {
-                        ttl.swap(j, j + 1);
-                    } else {
-                        break;
-                    }
-                }
-                return ret;
-            }
-        }
-        let hyoka = f();
-        self.nmiss += 1;
-        if self.sz < ENTRYSIZE {
-            self.sz += 1;
-        }
-        let idx = self.sz - 1;
-        ttl[idx].id = *id;
-        ttl[idx].hyoka = hyoka;
-        ttl[idx].hit = 1;
-        // ttl[self.sz - 1] = TTEntry::new(id, hyoka, 1);
-        // self.dumpsz();
-        hyoka
-    }
-
-    #[allow(dead_code)]
-    pub fn dumpsz(&self) {
-        for i in 0 .. 100 {
-        // for i in 0 .. ENTRYSIZE {
-                print!("{},", self.list[i].hit);
-        }
-        println!("hit,{},miss,{}", self.nhit, self.nmiss);
-    }
-}
-
-struct TTEntryv1 {
-    pub id : [u8 ; 16],
-    pub hyoka : f32,
-}
-
-impl TTEntryv1 {
-    pub fn new(i : &[u8 ; 16], h : f32) -> TTEntryv1 {
-        TTEntryv1 {
-            id : *i,
-            hyoka : h,
-        }
-    }
-}
-
-pub struct TranspositionTablev2 {
-    list : Vec< Vec<TTEntryv1> >,
-    nhit : usize,
-    nmiss : usize,
-}
-
-#[allow(dead_code)]
-impl TranspositionTablev2 {
-    pub fn new() -> TranspositionTablev2 {
-        let mut ret = TranspositionTablev2 {
-            list : Vec::with_capacity(256),
-            nhit : 0,
-            nmiss : 0,
-        };
-        ret.list.resize_with(256, || Vec::with_capacity(32));
-        ret
-    }
-
-    #[allow(dead_code)]
-    pub fn clear(&mut self) {
-        for l in self.list.iter_mut() {
-            l.clear();
-        }
-        self.nhit = 0;
-        self.nmiss = 0;
-    }
-
-    #[allow(dead_code)]
-    pub fn check(&self, id : &[u8 ; 16]) -> Option<f32> {
-        let lid = id[4] as usize;
-        let ttl = &self.list[lid];
-        let item = ttl.binary_search_by(|a| {
-            a.id.cmp(id)
-        });
-        match item {
-            Ok(t) => {
-                Some(ttl[t].hyoka)
-            },
-            Err(_) => {None}
+            // l.hit = 0;
+            l.hash = 0;
         }
     }
 
-    pub fn check_or_append<F>(&mut self, id : &[u8 ; 16], mut f : F) -> f32 where
-            F : FnMut() -> f32 {
-        let lid = id[4] as usize;
-        let ttl = &mut self.list[lid];
-        let item = ttl.binary_search_by(|a| {
-            a.id.cmp(id)
-        });
-        match item {
-            Ok(t) => {
-                // self.hit();
-                self.nhit += 1;
-                ttl[t].hyoka
-            },
-            Err(t) => {
-                let hyoka = f();
-                let n = ttl.len();
-                if n > MAXSIZE {
-                    // ttl.remove(0);
-                    ttl.pop();
-                }
-                ttl.insert(t, TTEntryv1::new(id, hyoka));
-                self.nmiss += 1;
-                hyoka
-            }
+    fn hash(b : &bitboard::BitBoard) -> u64 {
+        // 乱数テーブルや定数（適当に大きくて奇妙な値を使う）
+        const K1: u64 = 0x9e3779b185ebca87;
+        const K2: u64 = 0xc2b2ae3d27d4eb4f;
+        let mut h = b.black.wrapping_mul(K1) ^ b.white.wrapping_mul(K2);
+        // さらに混ぜる
+        h ^= h >> 33;
+        h = h.wrapping_mul(0xff51afd7ed558ccd);
+        h ^= h >> 33;
+        h = h.wrapping_mul(0xc4ceb9fe1a85ec53);
+        h ^= h >> 33;
+        h
+    }
+
+    pub fn check(&mut self, b : &bitboard::BitBoard) -> Option<f32> {
+        let h = Self::hash(b);
+        let idx = (h & (MAXSIZE - 1) as u64) as usize;
+        if self.list[idx].is_hit(b) {
+        // if self.list[idx].is_hash(h, b.teban) {
+            Some(self.list[idx].hyoka)
+        } else {
+            None
         }
     }
 
-    #[allow(dead_code)]
-    // twice slower than check_or_append()
-    pub fn check_or_append_poor<F>(&mut self, id : &[u8 ; 16], mut f : F) -> f32 where 
-            F : FnMut() -> f32 {
-        let lid = id[4] as usize;
-        let ttl = &mut self.list[lid];
-        let item = ttl.iter().position(|a| a.id.cmp(id).is_eq() );
-        match item {
-            Some(t) => {
-                self.nhit += 1;
-                ttl[t].hyoka
-            },
-            None => {
-                let hyoka = f();
-                let n = ttl.len();
-                if n > MAXSIZE {
-                    ttl.remove(0);
-                    // ttl.pop();
-                }
-                ttl.push(TTEntryv1::new(id, hyoka));
-                self.nmiss += 1;
-                hyoka
-            }
-        }
+    pub fn append(&mut self, b : &bitboard::BitBoard, hy : f32) {
+        let h = Self::hash(b);
+        let idx = (h & (MAXSIZE - 1) as u64) as usize;
+        self.list[idx] = TTEntry::from(h, b, hy);
     }
 
-    #[allow(dead_code)]
-    pub fn append(&mut self, id : &[u8 ; 16], hyoka : f32) {
-        let lid = id[4] as usize;
-        let ttl = &mut self.list[lid];
-        let n = ttl.len();
-        if n > MAXSIZE {
-            // ttl.remove(0);
-            ttl.pop();
-        }
-
-        let item = ttl.binary_search_by(|a| {
-            a.id.cmp(id)
-        });
-        let idx = match item {
-            Ok(t) => t,
-            Err(_) => ttl.len(),
-        };
-        ttl.insert(idx, TTEntryv1::new(id, hyoka));
-        // println!("tt.append size:{}, {:?}", n, id);
-    }
-
-    #[allow(dead_code)]
-    pub fn dumpsz(&self) {
-        // for i in 0.. 256 {
-        //     print!("{}:{},", i, self.list[i].len());
+    pub fn dump(&self) {
+        print!("ht:");
+        // for i in self.list.iter() {
+        //     print!("{},", i.hit);
         // }
-        println!("hit,{},miss,{}", self.nhit, self.nmiss);
-    }
-}
-
-#[allow(dead_code)]
-pub struct TranspositionTablev1 {
-    list : Vec<TTEntryv1>,
-}
-
-impl TranspositionTablev1 {
-    #[allow(dead_code)]
-    pub fn new() -> TranspositionTablev1 {
-        TranspositionTablev1 {
-            list : Vec::with_capacity(MAXSIZE)
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn clear(&mut self) {
-        self.list.clear();
-    }
-
-    #[allow(dead_code)]
-    pub fn check(&self, id : &[u8 ; 16]) -> Option<f32> {
-        let ttl = &self.list;
-        let item = ttl.binary_search_by(|a| {
-            a.id.cmp(id)
-        });
-        match item {
-            Ok(t) => {
-                Some(ttl[t].hyoka)
-            },
-            Err(_) => {None}
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn check_or_append<F>(&mut self, id : &[u8 ; 16], mut f : F) -> f32 where
-            F : FnMut() -> f32 {
-        let ttl = &mut self.list;
-        let item = ttl.binary_search_by(|a| {
-            a.id.cmp(id)
-        });
-        match item {
-            Ok(t) => {
-                ttl[t].hyoka
-            },
-            Err(t) => {
-                let hyoka = f();
-                let n = ttl.len();
-                if n > MAXSIZE {
-                    // ttl.remove(0);
-                    ttl.pop();
-                }
-                ttl.insert(t, TTEntryv1::new(id, hyoka));
-                hyoka
-            }
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn append(&mut self, id : &[u8 ; 16], hyoka : f32) {
-        let ttl = &mut self.list;
-
-        let n = ttl.len();
-        if n > MAXSIZE {
-            // ttl.remove(0);
-            ttl.pop();
-        }
-
-        let item = ttl.binary_search_by(|a| {
-            a.id.cmp(id)
-        });
-        let idx = match item {
-            Ok(t) => t,
-            Err(_) => ttl.len(),
-        };
-        ttl.insert(idx, TTEntryv1::new(id, hyoka));
-        // println!("tt.append size:{}, {:?}", n, id);
+        println!();
     }
 }
